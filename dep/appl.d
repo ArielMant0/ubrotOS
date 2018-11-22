bf8f0457d28e8936fb0c7c975c4baf74f189eef3
ArielMant0 <buecher.apps@gmail.com>
2018-11-22 11:51:57 +0100

    Removed build folders

--- dep/appl.d
+++
@@ -1,3 +0,0 @@
-build/appl.o: user/appl.cc user/appl.h device/cgastr.h object/o_stream.h \
- object/strbuf.h machine/cgascr.h machine/io_port.h device/keyboard.h \
- machine/keyctrl.h machine/key.h guard/gate.h machine/cpu.h
--- dep/guardian.d
+++
@@ -1,3 +0,0 @@
-build/guardian.o: guard/guardian.cc device/cgastr.h object/o_stream.h \
- object/strbuf.h machine/cgascr.h machine/io_port.h machine/plugbox.h \
- guard/gate.h
